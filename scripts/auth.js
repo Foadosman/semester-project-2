b23d7aa0d904f9bedec9310d20ea1f5575677200
Foadosman <foad.osman95@gmail.com>
2026-04-30 16:11:34 +0200

    Add login and register with localStorage
    - Implement user registration with API
    - Ipmelent login functionality
    - Store accesstoken and username in localStorage
    -Add redirect after login success
    -Style login and register(not quite happy, have to style further)

---
+++ scripts/auth.js
@@ -0,0 +1,70 @@
+import { API_BASE_URL } from "./api.js";
+
+const registerForm = document.getElementById("registerForm");
+
+if (registerForm) {
+    registerForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const name = document.getElementById("name").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const password = document.getElementById("password").value.trim();
+
+        try {
+            const response = await fetch(`${API_BASE_URL}/auth/register`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    password
+                })
+            });
+
+            const result = await response.json();
+
+            console.log("Register result:", result);
+        
+        } catch (error) {
+            console.error("Register failed:", error);
+        }
+    });
+}
+
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const email = document.getElementById("loginEmail").value.trim();
+        const password = document.getElementById("loginPassword").value.trim();
+
+        try {
+            const response = await fetch(`${API_BASE_URL}/auth/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+            });
+
+            const result = await response.json();
+
+            console.log("Login result", result);
+
+            const { accessToken, name } = result.data;
+            localStorage.setItem("token", accessToken);
+            localStorage.setItem("username", name);
+            window.location.href = "../index.html";
+        
+        } catch (error) {
+            console.error("Login failed:", error);
+        }
+    });
+}
